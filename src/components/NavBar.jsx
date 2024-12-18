2baa5ac12bcd2dfc7511135c44ef25847daed195
AmmanRizwan <ammanrizwan00007@gmail.com>
2024-12-18 12:43:21 +0530

    logo change

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,20 @@
 import React from "react";
-import { BsSend } from "react-icons/bs";
-import { VscAccount } from "react-icons/vsc";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from '../images/icon.png';
+import { FaUpload, FaUser } from "react-icons/fa";
 
 
 const NavBar = () => {
+  const location = useLocation();
+
+  const getBackground = (path) => {
+    return location.pathname === path ? "bg-[#211F1D]" : "";
+  }
   return (
-    <div className="text-white flex justify-between p-4 px-5  bg-[#2F2D29] items-center mb-10">
-      <Link to={'/user'}><div className="cursor-pointer"><VscAccount size={24} /></div></Link>
-      <Link to={'/'}><div className="font-medium text-md sm:text-xl flex items-center gap-2"><img src={logo} alt="python" className="w-10" /> TUTORIAL QUESTION</div></Link>
-      <Link to={'/senddata'}><div className="cursor-pointer"><BsSend size={24} /></div></Link>
+    <div className="text-white flex justify-between p-4 px-5 bg-[#2F2D29] items-center mb-10 transition-all duration-500">
+      <Link className={`${getBackground('/user')} rounded-3xl p-3`} to={'/user'}><div className={`cursor-pointer`}><FaUser size={24} /></div></Link>
+      <Link to={'/'}><div className="font-medium text-sm sm:text-xl flex items-center gap-2"><img src={logo} alt="python" className="w-6 sm:w-10" /> TUTORIAL QUESTION</div></Link>
+      <Link className={`${getBackground('/senddata')} rounded-3xl p-3`} to={'/senddata'}><div className={`cursor-pointer`}><FaUpload size={24} /></div></Link>
     </div>
   )
 }
