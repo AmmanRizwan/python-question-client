02926b175f775f796ae14c1c3fb171f1542e441a
AmmanRizwan <ammanrizwan00007@gmail.com>
2025-01-01 16:03:35 +0530

    margin issue

--- src/page/HowToUse.jsx
+++ src/page/HowToUse.jsx
@@ -3,7 +3,7 @@ import DEMO from '../data/demo.mp4';
 const HowToUse = () => {
   return (
     <section className="min-h-screen flex flex-col items-center text-white">
-      <div className="w-5/6">
+      <div className="w-5/6 my-20">
       <h1 className="font-semibold text-xl transition-all duration-500 sm:text-3xl">How To Use ?</h1>
       <p className="mt-3 sm:text-base text-sm">There is the small video which give you a quick understand how you can use this app by sending the code and question to store in our app.</p>
       <video className="grow my-10" controls>
